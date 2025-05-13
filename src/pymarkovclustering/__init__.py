d418ebb57e4ee9ca66c6b6bffe43d37f1b92d5c0
moshi <sy.41211@gmail.com>
2025-05-14 00:26:13 +0900

    Change v0.1.0 to v0.1.1

--- src/pymarkovclustering/__init__.py
+++ src/pymarkovclustering/__init__.py
@@ -11,7 +11,7 @@ from pymarkovclustering.utils import (
 )
 from pymarkovclustering.viz import easymclviz, mclviz
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "mcl",
     "easymcl",
